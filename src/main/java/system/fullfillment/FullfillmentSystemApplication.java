ecece9f694da77b5ec9e2265a9bab819c0fc6750
Boozooka <karhu00@list.ru>
2024-11-11 20:26:54 +0300

    Add Spring boot

--- src/main/java/system/fullfillment/FullfillmentSystemApplication.java
+++ src/main/java/system/fullfillment/FullfillmentSystemApplication.java
@@ -1,4 +1,11 @@
 package system.fullfillment;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FullfillmentSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FullfillmentSystemApplication.class);
+    }
 }
